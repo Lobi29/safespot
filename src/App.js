08bd1399e01e3efa6fccef3f2972fd6ef6b01115
chao lobi <gogoiladitya@gmail.com>
2023-04-28 19:36:22 +0530

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates nobis repudiandae libero veritatis accusamus voluptatum tenetur facere dolorem mollitia aliquid animi earum inventore itaque possimus labore, qui, facilis quibusdam dignissimos!</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores commodi cum culpa quam perspiciatis quidem debitis harum unde cupiditate veritatis animi, quasi ipsa dignissimos dolorem, iure a iste, itaque ullam.</h2>
+      <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, quae itaque! Eos laboriosam, iste ipsum distinctio iusto possimus odio nesciunt a sit voluptatem fugiat voluptas neque rem cum ad placeat.</h3>
+      <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem assumenda tempore minus. Quidem molestiae, a praesentium repellendus voluptas, laborum est perspiciatis nihil illo nisi facere deleniti eveniet tempora expedita harum. Assumenda odit dolore fuga, excepturi magnam numquam quas reprehenderit consectetur.</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, assumenda fugit unde fuga, voluptates officia ullam pariatur totam praesentium odit illo nostrum, eligendi dolor quae culpa similique voluptate autem eveniet asperiores eum doloribus tempore minus. Adipisci possimus nihil nulla nobis?</p>
+
     </div>
   );
 }
